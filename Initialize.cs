07e443715600f165766338a1b91bed6dacc80400
Patrick-G-W <patrick.g.ward@protonmail.com>
2020-08-21 16:43:12 +0100

    Finished unix initialization

--- Initialize.cs
+++ Initialize.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
@@ -10,51 +11,64 @@ namespace script_reader {
             Console.WriteLine("Generating new configuration...");
             Console.WriteLine("WARNING: A lot of files will be created in the directory of this program. Make sure you have moved the program to an appropriate location (not on your Desktop or Downloads folder). Proceed? (y or n).");
             if (Console.ReadLine()?.ToLower() == "y") {
-                generateConfig();
+                GenerateConfig();
             } else {
                 
             }
         }
 
-        static void generateConfig() {
+        static void GenerateConfig() {
             DirectoryInfo configDirectory = Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/config");
-            string python3Directory;
             if (Environment.OSVersion.Platform == PlatformID.Unix) {
-                if (unixCommand("python2", "-V").StartsWith("Python 2")) {
+                // os is unix based
+                if (Command("python2", "-V") != "err") {
                     Console.WriteLine("Python 2 is installed. Checking if Python 3 is also installed...");
-                    if (unixCommand("python3", "-V").StartsWith("Python 3")) {
+                    if (Command("python3", "-V").StartsWith("P")) {
                         Console.WriteLine("Python 3 is installed. Creating virtual environment...");
-                        unixCommand("python3", "-m venv venv");
+                        Command("python3", $"-m venv config/venv");
                         Console.WriteLine("Virtual environment created. Installing/downloading dependencies...");
-                        python3Directory = configDirectory + "/venv/bin/python";
-                        unixCommand("./venv/bin/python", "-m pip install unrpa");
+                        Command($"{configDirectory}/venv/bin/python", "-m pip install unrpa");
+                        Console.WriteLine("Installed unrpa.");
                         using (var webClient = new WebClient()) {
                             webClient.DownloadFile("https://raw.githubusercontent.com/CensoredUsername/unrpyc/master/unrpyc.py", $"{configDirectory}/unrpyc.py");
                         }
+                        Console.WriteLine("Downloaded unrpyc.py.");
                         Console.WriteLine("Dependencies installed/downloaded.");
+                        Console.WriteLine("Initialization finished. Ready!");
                     } else {
                         Console.WriteLine("Python 3 is either not installed.");
                     }
                 } else {
                     Console.WriteLine("Python 2 is either not installed.");
                 }
+            } else if (Environment.OSVersion.Platform == PlatformID.Win32NT) {
+                // os is windows based
+                Command("cmd.exe", "python -V");
+                Console.WriteLine("finished");
+            } else {
+                Console.WriteLine("unknown platform");
             }
         }
 
-        static string unixCommand(string exe, string args) {
-            var proc = new Process() {
-                StartInfo = new ProcessStartInfo {
-                    FileName = exe,
-                    Arguments = args,
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true
-                }
-            };
-            proc.Start();
-            string output = proc.StandardOutput.ReadToEnd();
-            proc.WaitForExit();
-            return output;
+        static string Command(string exe, string args) {
+            try {
+                var proc = new Process() {
+                    StartInfo = new ProcessStartInfo {
+                        FileName = exe,
+                        Arguments = args,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+                proc.Start();
+                string output = proc.StandardOutput.ReadToEnd();
+                proc.WaitForExit();
+                return output;
+            } catch (Win32Exception e) {
+                Console.WriteLine("An error has occured:");
+                throw;
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -7,38 +8,42 @@ using System.Net;
 namespace script_reader {
     class Program {
         static void Main(string[] args) {
+            /*try {
+                var proc = new Process() {
+                    StartInfo = new ProcessStartInfo {
+                        FileName = "pythonn",
+                        Arguments = "awd",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+                proc.Start();
+                string output = proc.StandardOutput.ReadToEnd();
+                proc.WaitForExit();
+                Console.WriteLine(proc.ExitCode);
+            } catch (Win32Exception e) {
+                Console.WriteLine(1);
+            }*/
             
-            /*var proc = new Process() {
-                StartInfo = new ProcessStartInfo {
-                    FileName = "./venv/bin/python",
-                    Arguments = "-m pip install unrpa",
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true
-                }
-            };
-            proc.Start();
-            string output = proc.StandardOutput.ReadToEnd();
-            proc.WaitForExit();
-            Console.WriteLine(output);
-            
-            Console.WriteLine(Environment.OSVersion.Platform);*/
-
             if (args[0] != "init") {
+                var folderExists = false;
                 foreach (var directory in Directory.GetDirectories(Directory.GetCurrentDirectory())) {
                     string dir = new DirectoryInfo(directory).Name;
                     if (dir == "config") {
-                        Console.WriteLine("Configuration already found. Would you like to generate new configuration (if your local config version is broken)? (y or n).");
-                        if (Console.ReadLine()?.ToLower() == "y") {
-                            Initialize init = new Initialize();
-                            break;
-                        } else {
-                            break;
-                        }
+                        folderExists = true;
                     }
                 }
-            } else {
-                
+
+                if (folderExists) {
+                    Console.WriteLine("Configuration already found. Would you like to generate new configuration (if your local config version is broken)? (y or n).");
+                    if (Console.ReadLine()?.ToLower() == "y") {
+                        Initialize init = new Initialize();
+                    }
+                } else {
+                    Console.WriteLine("No existing configuration found. Creating configuration...");
+                    Initialize init = new Initialize();
+                }
             }
         }
     }
