d3deefcda8ede2a193c4485300eb42d6da8af42e
Patrick-G-W <patrick.g.ward@protonmail.com>
2020-10-22 17:25:49 +0100

    Adding JSON response structure

---
+++ src/JsonStructure.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace script_reader {
+    public class Parent {
+        public int TotalWords;
+        public Time Time;
+    }
+
+    public class Time {
+        public int Hours;
+        public int Minutes;
+    }
+}
